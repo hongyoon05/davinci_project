f4a9a3c5fffa991338aec73e73a215fd4160d77b
김홍윤 <kimhongyoon@gimhong-yun-ui-MacBookAir.local>
2023-07-10 20:28:13 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: input },
+        ],
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 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', // Replace with your actual API key
+        },
+      });
+
+      const { choices } = response.data;
+      const { content } = choices[0].message;
+
+      setOutput(content);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Chat with ChatGPT</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+      <p>Response: {output}</p>
+    </div>
+  );
+}
+
+export default App;
